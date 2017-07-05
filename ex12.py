e048ba220882d601d356fb738e133ba4b6dcae49
Orion Ford <orionsbelt104@gmail.com>
2017-07-05 15:47:05 -0400

    examples 12-26

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = input("how old are you? ")
+height = input ("how tall are you? ")
+weight = input ("how much do you weight? ")
+
+print(f"So, you're {age} years old, {height} ft tall and {weight} pounds heavy.")
